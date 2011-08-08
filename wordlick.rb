a1e16d8ac3c983ac08bcedff39b93c3f4d2ca0e7
Diego Salazar <diego@greyrobot.com>
2011-08-08 17:58:33 -0400

    m

--- wordlick.rb
+++ wordlick.rb
@@ -9,11 +9,11 @@ post '/' do
   words  = File.read('./words').split "\n"
   params = env['rack.request.form_hash']
   @find, @except = params['find'], params['except']
-  @except        = @except.split('').join('|') unless @except.nil?
   any_letter     = '[a-zA-Z]{1}'
   regex          = /^(#{@find.split('').map { |f| f == '?' ? any_letter : f }.join ''})$/
   
-  @results = words.select { |w| w.match(regex) && !(/#{@except}/i.match(w)) }
+  @results = words.select { |w| w.match(regex) }
+  @results = @results.reject { |w| /#{@except}/i.match(w) } if @except && @except != ''
   
   erb :layout
 end
