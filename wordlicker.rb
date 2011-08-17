ff631e466d8c27b6652642f9eafe0a671afaf610
Diego Salazar <diego@greyrobot.com>
2011-08-17 14:55:16 -0400

    fixed bug in suggest when no results

--- wordlicker.rb
+++ wordlicker.rb
@@ -67,7 +67,7 @@ class Wordlicker
     end
     
     def suggest!
-      return if words.empty?
+      return if @words.empty?
       @counts = {}
       @words.each do |word|
         word.split('').each do |letter|
