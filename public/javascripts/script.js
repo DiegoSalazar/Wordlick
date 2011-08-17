76789bb7d11958323638ae1ee702397b0a429181
Diego Salazar <diego@greyrobot.com>
2011-08-17 14:57:56 -0400

    doc ready in js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,25 +1,26 @@
 /* Author: Diego Salazar
    Wordlick: Hangman solver
 */
+$(function() {
+	// Tab switcher
+	$('.tab').each(function() {
+		var active_tab = $('a.active', '#tab_toggle'),
+			href_parts = active_tab.attr('href').split('#'),
+			hash	   = href_parts[href_parts.length - 1];
 
-// Tab switcher
-$('.tab').each(function() {
-	var active_tab = $('a.active', '#tab_toggle'),
-		href_parts = active_tab.attr('href').split('#'),
-		hash	   = href_parts[href_parts.length - 1];
-	
-	$('.tab').hide();
-	$('#'+ hash).show();
-});
-$('a', '#tab_toggle').click(function() {
-	var href_parts = this.href.split('#'),
-		hash	   = href_parts[href_parts.length - 1];
-	
-	$('.tab').hide();
-	$('#'+ hash).show();
-	
-	$('a', '#tab_toggle').removeClass('active');
-	$(this).addClass('active');
-	
-	return false;
-});
+		$('.tab').hide();
+		$('#'+ hash).show();
+	});
+	$('a', '#tab_toggle').click(function() {
+		var href_parts = this.href.split('#'),
+			hash	   = href_parts[href_parts.length - 1];
+
+		$('.tab').hide();
+		$('#'+ hash).show();
+
+		$('a', '#tab_toggle').removeClass('active');
+		$(this).addClass('active');
+
+		return false;
+	});
+})
